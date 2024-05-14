8bbb6ebb15bded40a75883c7bd18ddc812dc6089
Muhammad Fauzan Aristya Putra <muhammadfauzanaristyaputra@gmail.com>
2024-05-14 09:10:23 +0300

    Add transparent parking overlays

--- geojson_plotter.py
+++ geojson_plotter.py
@@ -1,11 +1,14 @@
 import os
 import re
 import subprocess
+import cv2 as cv
 import numpy as np
 import argparse
 import cameratransform as ct
 
-from geojson import Polygon, dumps
+from geojson import Polygon, dumps, loads
+
+PARKING_TRANSPARENCY = 0.25
 
 parser = argparse.ArgumentParser(
 	description='This sample demonstrates Lucas-Kanade Optical Flow calculation. \
@@ -16,18 +19,27 @@ parser.add_argument('images', type=str, help='path to image files', default="\\d
 args = parser.parse_args()
 
 # Create output directory
-if not os.path.exists(f'.\\output\\geojson\\{args.images}'): os.makedirs(f'.\\output\\geojson\\{args.images}')
+if not os.path.exists(f'.\\output\\camera\\{args.images}'): os.makedirs(f'.\\output\\camera\\{args.images}')
+if not os.path.exists(f'.\\output\\parking\\{args.images}'): os.makedirs(f'.\\output\\parking\\{args.images}')
 
 # Get the number of images in the directory
 cwd = os.getcwd()
 num_images = len([name for name in os.listdir(f"{cwd}\\data\\{args.images}") if os.path.isfile(os.path.join(f"{cwd}\\data\\{args.images}", name))])
-# process = subprocess.Popen(['exiftool', f"{cwd}\\data\\{args.images}\\00000.jpg"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
+# Load the parking geojson file
+parking = None
+with open(f'.\\data\\{args.images}\\parking.geojson', 'r') as f:
+	parking = f.read()
+	parking = loads(parking)
 
 img_index = 0
 while(1):
 
 	# If the image does not exist, break the loop
 	if not os.path.exists(f"{cwd}\\data\\{args.images}\\{"{:05d}".format(img_index)}.jpg"): break
+
+	# Read the image
+	img = cv.imread(f"{cwd}\\data\\{args.images}\\{"{:05d}".format(img_index)}.jpg")
 
 	# Create process to use ExifTool
 	process = subprocess.Popen(['exiftool', f"{cwd}\\data\\{args.images}\\{"{:05d}".format(img_index)}.jpg"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
@@ -50,8 +62,6 @@ while(1):
 	# Get the GPS data
 	gps = info_dict['GPS Position'].replace(' deg', '°')
 	lat, lon = ct.gpsFromString(gps)
-	# lat = parse(info_dict['GPS Latitude'])
-	# lon = parse(info_dict['GPS Longitude'])
 	alt = float(re.sub('[^0-9.-]', '', info_dict['Relative Altitude']))
 	# print("Latitude:", lat, "Longitude:", lon, "Altitude:", alt)
 
@@ -84,9 +94,21 @@ while(1):
 	points.append((coords[0][1], coords[0][0]))
 	polygon = Polygon([points], precision=8)
 
-	# Write the GeoJSON polygon to a file
-	with open(f'.\\output\\geojson\\{args.images}\\{"{:05d}".format(img_index)}.geojson', 'w') as f:
+	# Write the camera polygon to a GeoJSON file
+	with open(f'.\\output\\camera\\{args.images}\\{"{:05d}".format(img_index)}.geojson', 'w') as f:
 		f.write(dumps(polygon))
+
+	# Project parking polygon coordinates to image coordinates
+	projected_coords = []
+	for coord in parking.get('features')[0].get('geometry').get('coordinates')[0]:
+		x, y = cam.imageFromGPS(np.array([coord[1], coord[0]]))
+		projected_coords.append([x, y])
+
+	# Draw the parking polygon on the image and save it
+	overlay = img.copy()
+	cv.fillPoly(overlay, [np.array(projected_coords, np.int32)], (0, 255, 0, PARKING_TRANSPARENCY * 255))
+	cv.addWeighted(overlay, PARKING_TRANSPARENCY, img, 1-PARKING_TRANSPARENCY, 0, img)
+	cv.imwrite(f'.\\output\\parking\\{args.images}\\{"{:05d}".format(img_index)}.jpg', img)
 
 	print(f'Frame {img_index + 1}/{num_images} processed ({round((img_index + 1) / num_images * 100, 2)}%)...')
 	img_index += 1
